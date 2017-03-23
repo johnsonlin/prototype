32c52eaf86cc9796f92b7c12513ae51db1a6eaca
Johnson Lin <konglong_1984@msn.com>
2017-03-23 15:48:22 +1100

    remove nav in landing page; add form to landing page; add package.json; add about page; disable chat demo on landing page; update map location in contact page;

--- HTML/assets/js/custom.js
+++ HTML/assets/js/custom.js
@@ -1,4 +1,7 @@
 /* Write here your custom javascript codes */
-(function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
-  t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+(function (w,i,d,g,e,t,s) {
+  if (!w.location.toString().match(/landing/)) {
+    w[d] = w[d]||[];t= i.createElement(g);
+    t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+  }
 })(window, document, '_gscq','script','//widgets.getsitecontrol.com/76542/script.js');
---
+++ server.js
@@ -0,0 +1,6 @@
+const liveServer = require('live-server');
+
+liveServer.start({
+  root: './HTML',
+  file: 'taas_home.html'
+});
